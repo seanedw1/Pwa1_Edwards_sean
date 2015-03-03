d72f8d82b7b98fa5dcdfd1100eed172d9052b4f4
seanedw1 <seanpro@Seans-MacBook-Pro-2.local>
2015-03-03 10:17:33 -0500

    up

--- PWA1-Initial_PreTest/PreTest_Layout_Assets/js/script.js
+++ PWA1-Initial_PreTest/PreTest_Layout_Assets/js/script.js
@@ -1,3 +1,14 @@
 //sean edwards
 //3/3/15
-//
+//\
+
+alert("Welcome to our store");
+
+
+prompt("cost of item");
+
+prompt("discount percentage");
+
+console.log("hello sean");
+
+alert("hello, your orignal price was with the discount of discount applied your new total is fCost");
